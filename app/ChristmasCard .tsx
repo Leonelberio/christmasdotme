f971fc7790bb00b045c78656b36b546308f4bd81
Leandro ADAGBE <67783416+Leonelberio@users.noreply.github.com>
2024-12-25 12:10:52 +0000

    [ADDED] Added Github repo link to the footer

--- app/ChristmasCard .tsx
+++ app/ChristmasCard .tsx
@@ -379,14 +379,34 @@ Créez aussi votre carte personnalisée pour vos amis sur : https://christmas.le
       </div>
       <footer className="bg-gray-900 text-white py-4 flex flex-col items-center pt-6">
         <p className="mb-2">© 2024 Christmas.me. Tous droits réservés.</p>
-        <a
-          href="https://www.linkedin.com/in/leoneladagbe/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:underline flex items-center"
-        >
-          Connectez-vous avec moi sur LinkedIn
-        </a>
+        <div className="flex items-center space-x-4">
+          {/* LinkedIn Link */}
+          <a
+            href="https://www.linkedin.com/in/leoneladagbe/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline flex items-center"
+          >
+            Connectez-vous avec moi sur LinkedIn
+          </a>
+          {/* GitHub Link */}
+          <a
+            href="https://github.com/Leonelberio/christmasdotme"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-400 flex items-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              className="w-6 h-6"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577v-2.045c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.757-1.333-1.757-1.089-.744.083-.729.083-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.997.108-.775.418-1.305.762-1.605-2.665-.305-5.467-1.334-5.467-5.93 0-1.31.47-2.381 1.236-3.221-.123-.303-.536-1.523.117-3.176 0 0 1.008-.322 3.301 1.23a11.484 11.484 0 0 1 3.004-.404 11.48 11.48 0 0 1 3.004.404c2.293-1.552 3.301-1.23 3.301-1.23.653 1.653.24 2.873.118 3.176.768.84 1.236 1.911 1.236 3.221 0 4.61-2.807 5.623-5.479 5.92.43.372.823 1.102.823 2.222v3.293c0 .319.217.694.824.576C20.565 22.092 24 17.593 24 12.297c0-6.627-5.373-12-12-12z" />
+            </svg>
+            <span className="ml-2">GitHub</span>
+          </a>
+        </div>
       </footer>
     </div>
   );
