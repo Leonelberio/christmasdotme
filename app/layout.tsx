4ddc9948d72292754dc525b8ed7f1f591409b241
Leandro ADAGBE <67783416+Leonelberio@users.noreply.github.com>
2024-12-25 11:50:24 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,6 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          defer
+          data-website-id="672ba979a07d7e1c0e073742"
+          data-domain="lesprosdelatech.com"
+          src="https://datafa.st/js/script.js"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
